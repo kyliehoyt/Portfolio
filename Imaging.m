e1e3a210ad14d353a1ffba2a1133d28c65accadb
kyliehoyt <60266803+kyliehoyt@users.noreply.github.com>
2022-10-17 22:14:22 -0500

    Delete Imaging.m

--- Imaging.m
+++
@@ -1,9 +0,0 @@
-
-n = 1058;
-imdata = imread('world.jpg');
-s = imdata(:,:,2);
-s = 1*(s<0);
-s = reshape(s,[n,n]);
-[u,sig,v] = svd(s);
-num = 40;
-imshow(u(:,1:num)*sig(1:num,1:num)*v(:,1:num)',[0 1])
